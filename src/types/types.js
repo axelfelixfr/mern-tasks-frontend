ac7494ea8651793216bf69d689f3aa7a8ceb7f59
Axel <axelfelix123@hotmail.com>
2021-08-30 19:52:18 -0500

    Cambios en lista de proyectos

--- src/types/types.js
+++ src/types/types.js
@@ -5,18 +5,4 @@ export const types = {
   newProject: '[Projects] New project',
   selectProject: '[Projects] Select project',
   deleteProject: '[Projects] Delete project'
-
-  // login: '[Auth] Login',
-  // logout: '[Auth] Logout',
-
-  // uiStartLoading: '[UI] Start loading',
-  // uiFinishLoading: '[UI] Finish loading',
-
-  // notesAddNew: '[Notes] New note',
-  // notesActive: '[Notes] Set active note',
-  // notesLoad: '[Notes] Load notes',
-  // notesUpdated: '[Notes] Update note',
-  // notesFileUrl: '[Notes] Updated image url',
-  // notesDelete: '[Notes] Delete note',
-  // notesLogoutCleaning: '[Notes] Logout Cleaning'
 };
